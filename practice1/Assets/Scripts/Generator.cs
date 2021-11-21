c3baec552366a82ce6c194faf37784dfcf91255d
justin0u0 <mail@justin0u0.com>
2021-11-22 06:11:21 +0800

    feat: add practice1

---
+++ practice1/Assets/Scripts/Generator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public GameObject prefabCube;
+    private GameObject cube;
+
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (cube == null) {
+            float x = Random.Range(-5.0f, 5.0f);
+            float z = Random.Range(-5.0f, 5.0f);
+            cube = Instantiate(prefabCube, new Vector3(x, 0.5f, z), Quaternion.identity);
+        }
+    }
+}
---
+++ practice1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody rb;
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    void FixedUpdate() {
+        float moveVertical = Input.GetAxis("Vertical");
+        float speed = 3.0f;
+        rb.velocity = gameObject.transform.forward * moveVertical * speed;
+
+        if (Input.GetKey(KeyCode.A)) {
+            gameObject.transform.Rotate(new Vector3(0, -2.0f, 0));
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            gameObject.transform.Rotate(new Vector3(0, 2.0f, 0));
+        }
+
+        rb.angularVelocity = Vector3.zero;
+    }
+}
