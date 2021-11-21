a6d9c53e54e2ca36ebed7786bd8ca3435fe16728
justin0u0 <mail@justin0u0.com>
2021-11-22 06:10:32 +0800

    feat: add lab3

---
+++ lab3/Assets/Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float speed = 30.0f;
+
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+    
+    void FixedUpdate() {
+        if(Input.GetKey(KeyCode.UpArrow)) {
+            transform.position += new Vector3(0 ,speed * Time.deltaTime, 0);
+        }
+        if(Input.GetKey(KeyCode.RightArrow)) {
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)) {
+            transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.DownArrow)) {
+            transform.position -= new Vector3(0, speed * Time.deltaTime, 0);
+        }
+        if(Input.GetKey(KeyCode.W)) {
+            GetComponent<Camera>().fieldOfView += 5;
+        }
+        if(Input.GetKey(KeyCode.S)) {
+            GetComponent<Camera>().fieldOfView -= 5;
+        }
+    }
+}
